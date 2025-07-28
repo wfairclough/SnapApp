4382f538bc0061a79ca2798a415fd7374dd374cc
Will Fairclough <github@willgo.dev>
2025-07-27 20:26:58 -0400

    Initial Commit

---
+++ SnapApp/SnapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapAppApp.swift
+//  SnapApp
+//
+//  Created by Will Fairclough on 2025-07-27.
+//
+
+import SwiftUI
+
+@main
+struct SnapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnapAppTests/SnapAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnapAppTests.swift
+//  SnapAppTests
+//
+//  Created by Will Fairclough on 2025-07-27.
+//
+
+import Testing
+@testable import SnapApp
+
+struct SnapAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
